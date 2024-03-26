baba3fdaed4376929535a524216a3f9017bdacc2
Thiago Lima <thlima.silva.souza@gmail.com>
2024-03-26 14:51:51 -0300

    creating file with list of tips videos

---
+++ videosTips.js
@@ -0,0 +1,37 @@
+export default [
+{ 
+    src: "https://www.youtube.com/embed/qGTQEuSTVZw?si=fNrPWoC5UdJob4-D",
+    title: "Comprar uma bike sem ser enganado"
+},
+
+{ 
+    src: "https://www.youtube.com/embed/bt6QTGMcGGw?si=KlYM1ugHINGt4Wb5",
+    title: "Montando sua nova bicicleta que veio na caixa"
+},
+
+{ 
+    src: "https://www.youtube.com/embed/h73w942U91I?si=h9BNdJJUiA3wPYQg",
+    title: "Trocando o pneu da bicicleta"
+},
+
+{ 
+    src: "https://www.youtube.com/embed/PY8GEQ9g5Rk?si=f4dF6M2fqdfySWlq",
+    title: "Como se preparar para pedalar maiores distâncias?"
+},
+
+{ 
+    src: "https://www.youtube.com/embed/39nVMt-SxKs?si=MEmR9-Yb7IhF5o-k",
+    title: "Como passar marchas no rapidfire?"
+},
+
+{ 
+    src: "https://www.youtube.com/embed/c-unBuZvLPY?si=-5EqP0-KLkTURNJj",
+    title: "Quais manutenções devo fazer na bike após pedalar na chuva?"
+},
+
+{ 
+    src: "https://www.youtube.com/embed/csDPW0TdApk?si=PtPuzEZ9xO5nKw6Y",
+    title: "Como calibrar pneus da MTB corretamente?"
+}
+   
+]
