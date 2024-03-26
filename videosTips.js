f2c1801c49f9dbcc98fe24428628d682eb140e09
Thiago Lima <thlima.silva.souza@gmail.com>
2024-03-26 18:22:09 -0300

    update file videosTips.js

--- videosTips.js
+++ videosTips.js
@@ -1,37 +1,44 @@
-export default [
-{ 
-    src: "https://www.youtube.com/embed/qGTQEuSTVZw?si=fNrPWoC5UdJob4-D",
-    title: "Comprar uma bike sem ser enganado"
-},
+const videoList = [
+    {
+        url: "https://www.youtube.com/embed/qGTQEuSTVZw?si=fNrPWoC5UdJob4-D",
+        thumbnail: "https://img.youtube.com/vi/qGTQEuSTVZw/mqdefault.jpg",
+        title: "Comprar uma bike sem ser enganado"
+    },
 
-{ 
-    src: "https://www.youtube.com/embed/bt6QTGMcGGw?si=KlYM1ugHINGt4Wb5",
-    title: "Montando sua nova bicicleta que veio na caixa"
-},
+    {
+        url: "https://www.youtube.com/embed/bt6QTGMcGGw?si=KlYM1ugHINGt4Wb5",
+        thumbnail: "https://img.youtube.com/vi/bt6QTGMcGGw/mqdefault.jpg",
+        title: "Montando sua nova bicicleta que veio na caixa"
+    },
 
-{ 
-    src: "https://www.youtube.com/embed/h73w942U91I?si=h9BNdJJUiA3wPYQg",
-    title: "Trocando o pneu da bicicleta"
-},
+    {
+        url: "https://www.youtube.com/embed/h73w942U91I?si=h9BNdJJUiA3wPYQg",
+        thumbnail: "https://img.youtube.com/vi/h73w942U91I/mqdefault.jpg",
+        title: "Trocando o pneu da bicicleta"
+    },
 
-{ 
-    src: "https://www.youtube.com/embed/PY8GEQ9g5Rk?si=f4dF6M2fqdfySWlq",
-    title: "Como se preparar para pedalar maiores distâncias?"
-},
+    {
+        url: "https://www.youtube.com/embed/PY8GEQ9g5Rk?si=f4dF6M2fqdfySWlq",
+        thumbnail: "https://img.youtube.com/vi/PY8GEQ9g5Rk/mqdefault.jpg",
+        title: "Como se preparar para pedalar maiores distâncias?"
+    },
 
-{ 
-    src: "https://www.youtube.com/embed/39nVMt-SxKs?si=MEmR9-Yb7IhF5o-k",
-    title: "Como passar marchas no rapidfire?"
-},
+    {
+        url: "https://www.youtube.com/embed/39nVMt-SxKs?si=MEmR9-Yb7IhF5o-k",
+        thumbnail: "https://img.youtube.com/vi/39nVMt-SxKs/mqdefault.jpg",
+        title: "Como passar marchas no rapidfire?"
+    },
 
-{ 
-    src: "https://www.youtube.com/embed/c-unBuZvLPY?si=-5EqP0-KLkTURNJj",
-    title: "Quais manutenções devo fazer na bike após pedalar na chuva?"
-},
+    {
+        url: "https://www.youtube.com/embed/c-unBuZvLPY?si=-5EqP0-KLkTURNJj",
+        thumbnail: "https://img.youtube.com/vi/c-unBuZvLPY/mqdefault.jpg",
+        title: "Quais manutenções devo fazer na bike após pedalar na chuva?"
+    },
 
-{ 
-    src: "https://www.youtube.com/embed/csDPW0TdApk?si=PtPuzEZ9xO5nKw6Y",
-    title: "Como calibrar pneus da MTB corretamente?"
-}
-   
-]
+    {
+        url: "https://www.youtube.com/embed/u-qPvuq7kAA?si=FpyzjvRefeNQE4ur",
+        thumbnail: "https://img.youtube.com/vi/u-qPvuq7kAA/mqdefault.jpg",
+        title: "Qual o tamanho ideal do quadro da bike aro 29?"
+    }
+
+];
